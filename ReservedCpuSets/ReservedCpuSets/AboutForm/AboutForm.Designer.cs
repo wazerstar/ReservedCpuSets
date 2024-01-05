dd9155c850a916b8d204ef45e66aa75863cf1568
AMIT <86924251+amitxv@users.noreply.github.com>
2024-01-05 15:42:43 +0000

    Update version

--- ReservedCpuSets/ReservedCpuSets/AboutForm/AboutForm.Designer.cs
+++ ReservedCpuSets/ReservedCpuSets/AboutForm/AboutForm.Designer.cs
@@ -45,7 +45,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(99, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Version 0.1.7 64-Bit";
+            this.label2.Text = "Version 0.1.8 64-Bit";
             // 
             // gitHubLink
             // 
