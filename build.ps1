2e06ce20ba0a77e34e23c83fa1ead3e8246e1495
AMIT <86924251+amitxv@users.noreply.github.com>
2024-03-06 10:35:13 +0000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,17 +1,19 @@
 function main() {
     # build application
-    MSBuild.exe .\ReservedCpuSets\ReservedCpuSets.sln -p:Configuration=Release -p:Platform=x64
+    MSBuild.exe ".\ReservedCpuSets\ReservedCpuSets.sln" -p:Configuration=Release -p:Platform=x64
     # build DLL
-    MSBuild.exe .\ReservedCpuSetsDLL\ReservedCpuSets.sln -p:Configuration=Release -p:Platform=x64
+    MSBuild.exe ".\ReservedCpuSetsDLL\ReservedCpuSets.sln" -p:Configuration=Release -p:Platform=x64
 
-    if (Test-Path "build") {
-        Remove-Item -Path "build" -Recurse
+    if (Test-Path ".\build\") {
+        Remove-Item -Path ".\build\" -Recurse
     }
 
     # create folder structure
-    New-Item -ItemType Directory -Path build\ReservedCpuSets
-    Move-Item .\ReservedCpuSets\ReservedCpuSets\bin\x64\Release\ReservedCpuSets.exe build\ReservedCpuSets
-    Move-Item .\ReservedCpuSetsDLL\x64\Release\ReservedCpuSets.dll build\ReservedCpuSets
+    New-Item -ItemType Directory -Path ".\build\ReservedCpuSets\"
+
+    # create final package
+    Move-Item ".\ReservedCpuSets\ReservedCpuSets\bin\x64\Release\ReservedCpuSets.exe" ".\build\ReservedCpuSets\"
+    Move-Item ".\ReservedCpuSetsDLL\x64\Release\ReservedCpuSets.dll" ".\build\ReservedCpuSets\"
 
     return 0
 }
