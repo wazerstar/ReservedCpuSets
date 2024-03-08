5b9b24fa399e3b55515e4055f16a749867c396f5
AMIT <86924251+amitxv@users.noreply.github.com>
2024-03-08 11:20:39 +0000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -9,17 +9,17 @@ function main() {
         return 1
     }
 
-    # build application
-    MSBuild.exe ".\ReservedCpuSets\ReservedCpuSets.sln" -p:Configuration=Release -p:Platform=x64
-    # build DLL
-    MSBuild.exe ".\ReservedCpuSetsDLL\ReservedCpuSets.sln" -p:Configuration=Release -p:Platform=x64
-
     if (Test-Path ".\build\") {
         Remove-Item -Path ".\build\" -Recurse -Force
     }
 
     # create folder structure
-    New-Item -ItemType Directory -Path ".\build\ReservedCpuSets\"
+    mkdir ".\build\ReservedCpuSets\"
+
+    # build application
+    MSBuild.exe ".\ReservedCpuSets\ReservedCpuSets.sln" -p:Configuration=Release -p:Platform=x64
+    # build DLL
+    MSBuild.exe ".\ReservedCpuSetsDLL\ReservedCpuSets.sln" -p:Configuration=Release -p:Platform=x64
 
     # create final package
     Copy-Item ".\ReservedCpuSets\ReservedCpuSets\bin\x64\Release\ReservedCpuSets.exe" ".\build\ReservedCpuSets\"
